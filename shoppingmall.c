567526feca2bc002e1e5b4663ffabf16b8fdbe28
sixxi-winter <sinbi4005@gmail.com>
2024-09-26 11:49:24 +0900

    Add files via upload

--- shoppingmall.c
+++ shoppingmall.c
@@ -1,56 +1,72 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-#define MAX_PRODUCTS 100
 
 int main()
 {
-    int count;
-    int stock[MAX_PRODUCTS];
-    int id, sold, i;
+    int type, receive, sale, ID;
+    int total_receive[100];
+    int total_sale[100];
+    int total = 0;
+    int proportion;
+    int sum_receive = 0;
+    int most = 0;
+    int min = 0;
+    int id;
 
-    do {
-        printf("상품 개수(종류)입력 (1 ~ 100): ");
-        scanf("%d", &count);
-    } while (count < 1 || count > 100);
-
+    printf("상품 개수(종류)입력: ");
+    scanf_s("%d", &type);
     printf("상품 별 입고수량 입력: ");
-    for (i = 0; i < count; i++) {
-        scanf("%d", &stock[i]);
+    for (int i = 0; i < type; i++)
+    {
+        scanf_s("%d", &receive);
+        total_receive[i] = receive;
+    }
+    printf("상품 별 판매수량 입력: ");
+    for (int j = 0; j < type; j++)
+    {
+        scanf_s("%d", &sale);
+        total_sale[j] = sale;
     }
 
-    printf("상품 별 판매수량을 입력: ");
-    for (i = 0; i < count; i++) {
-        scanf("%d", &sold);
-        if (sold <= stock[i]) {
-            stock[i] -= sold;
-        }
-        else {
-            printf("판매 수량이 재고보다 많음. 최대 판매 가능 수량: %d\n", stock[i]);
+    printf("재고 수량: ");
+    for (int k = 0; k < type; k++)
+    {
+        printf("%d ", total_receive[k] - total_sale[k]);
+    }
+    printf("\n총 판매량: ");
+    for (int i = 0; i < type; i++)
+    {
+        total += total_sale[i];
+        sum_receive += total_receive[i];
+    }
+    printf("%d (판매율 %.2f%%)\n", total, ((double)total / sum_receive) * 100);
+    printf("가장 많이 판매된 상품: ID ");
+    for (int i = 0; i < type; i++)
+    {
+        if (most < total_sale[i])
+        {
+            most = total_sale[i];
+            id = i + 1;
         }
     }
-
-    while (1) {
-        printf("재고 수량을 확인할 상품 ID를 입력 (1 ~ %d, 종료는 0): ", count);
-        scanf("%d", &id);
-
-        if (id == 0) {
-            break;
-        }
-        else if (id > 0 && id <= count) {
-            printf("상품 ID %d의 남은 재고 수량: %d\n", id, stock[id - 1]);
-            printf("모든 상품의 재고 수량: ");
-            for (i = 0; i < count; i++) {
-                printf("%d ", stock[i]);
-            }
-            printf("\n");
-        }
-        else {
-            printf("잘못된 ID입니다. 다시 입력\n");
+    printf("%d, 판매량 %d\n", id, most);
+    printf("가장 적게 판매된 상품: ID ");
+    min = total_sale[0];
+    for (int i = 0; i < type; i++)
+    {
+        if (min > total_sale[i])
+        {
+            min = total_sale[i];
+            id = i + 1;
         }
     }
-
-
+    printf("%d, 판매량 %d\n", id, min);
+    for (int i = 0; i < type; i++)
+    {
+        if (total_receive[i] - total_sale[i] <= 2)
+        {
+            printf("상품 ID %d: 재고부족(%d)\n", i + 1, total_receive[i] - total_sale[i]);
+        }
+    }
 
     return 0;
 }
