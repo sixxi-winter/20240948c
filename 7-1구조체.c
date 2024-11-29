6254729721a65b396f546e272b96b0834df1ae17
sixxi-winter <sinbi4005@gmail.com>
2024-11-29 19:31:14 +0900

    Add files via upload

---
+++ 7-1구조체.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define SIZE 100
+
+typedef struct point{
+    int x;
+    int y;
+} POINT;
+
+int main()
+{
+    POINT point[SIZE] = {0};
+    POINT temp = {0, 0};
+    int least;
+    
+    srand(time(NULL));
+    for(int i = 0; i < SIZE; i++){
+        point[i].x = rand() % 101;
+        point[i].y = rand() % 101;
+    }
+    
+    for(int i = 0; i < SIZE; i++){
+        printf("point[%3d] : (%3d, %3d)\n", i, point[i].x, point[i].y);
+    }
+    
+    for (int i = 0; i < SIZE - 1; i++){
+        least = i;
+        for(int j = i + 1; j < SIZE; j++){
+            if(point[least].x > point[j].x) least = j;
+            else if(point[least].x == point[j].x && point[least].y > point[j].y)
+                least = j;
+        }
+        temp = point[least];
+        point[least] = point[i];
+        point[i] = temp;
+    }
+    
+    printf("after sorting >>>>>>>>>>>>>>>>>>>>>>\n");
+    
+    for(int i = 0; i < SIZE; i++){
+        printf("point[%3d] : (%3d, %3d)\n", i, point[i].x, point[i].y);
+    }
+
+    return 0;
+}
