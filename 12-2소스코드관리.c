9394cc6ea87abd87ff6773da183c4409c2a79e97
sixxi-winter <sinbi4005@gmail.com>
2024-12-05 07:41:29 +0900

    Add files via upload

---
+++ 12-2소스코드관리.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include "calculator.h"
+#include "something.h"
+
+int main()
+{
+    printf("%d", printAdd(10, 20));
+
+    return 0;
+}
